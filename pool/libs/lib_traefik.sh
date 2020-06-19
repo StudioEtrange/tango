c363ebab2ab5acfbcd2ad115fc30049e8fcce776
StudioEtrange <nomorgan@gmail.com>
2020-06-19 09:17:09 +0200

    update

--- pool/libs/lib_traefik.sh
+++ pool/libs/lib_traefik.sh
@@ -1,60 +1,62 @@
 # Standard GET API https://docs.traefik.io/operations/api
 # curl -kL http://traefik.domain.com:9000/api/version
-# REST API https://docs.traefik.io/v1.7/configuration/backends/rest/
-# API REST PUT request sample
-# curl -kL -u tango:tango -d "{}" -X PUT https://traefik.domain.com:9000/api/providers/rest
+# curl -kL http://traefik.domain.com:9000/api/rawdata
 
+
+
+# REST API https://docs.traefik.io/v1.7/configuration/backends/rest/
+# request sample 1 :
+#           curl -kL -u tango:tango -d "{}" -X PUT https://traefik.domain.com:9000/api/providers/rest
+# request sample 2 :
+# there is a direct mapping from labels to json. When label is a list (like entrypoints) it is translated as a json array [ ]
+# sample to add a router
+#      - "traefik.http.routers.test.entrypoints=entrypoint1"
+#      - "traefik.http.routers.test.rule=HostRegexp(`{subdomain:test.}{domain:domain.com}`)"
+#      - "traefik.http.routers.test.service=service1@docker"
+# body='
+# {
+#     "http": { 
+#         "routers": {
+#             "test" : {
+#                 "entryPoints": [
+#                     "entrypoint1"
+#                 ],
+#                 "rule": "HostRegexp(`{subdomain:test.}{domain:domain.com}`)",
+#                 "service": "service1@docker"
+#             }
+#         }
+#     }
+# }
+# '
+# curl -d "${body}" -k -u user:password -X PUT https://traefik.domain.com:9000/api/providers/rest
 
 __traefik_api_url() {
     TRAEFIK_INTERNAL_API_URL="http://traefik/api"
 }
 
 
-
+# __traefik_api_request "GET" "rawdata"
+# __traefik_api_request "PUT" "{}"
 __traefik_api_request() {
-    local __request="$1"
-    local __http_command="$2"
+    local __http_command="$1"
+    local __request="$2"
     local __url="$TRAEFIK_INTERNAL_API_URL"
+
+    local __body=
 
     [ "${__http_command}" = "" ] && __http_command="GET"
     
     case $__http_command in
-        GET ) __url="${__url}"
+        GET ) 
+            __url="${__url}"
+            [ ! "${__request}" = "" ] && __url="${__url}/${__request}"
         ;;
-        PUT ) __url="${__url}/providers/rest"
+        PUT ) 
+            __url="${__url}/providers/rest"
+            __body="${__request}"
         ;;
     esac
-    docker run --network "${TANGO_APP_NETWORK_NAME}" --rm curlimages/curl:7.70.0 curl -u ${TRAEFIK_API_USER}:${TRAEFIK_API_HASH_PASSWORD} -X ${__http_command} -skL "${__url}"
-}
 
-
-__traefik_api_set_auth_service() {
-    local __service="$1"
-    local __group_id="$2"
-
-    # CONTINUE HERE
-    # see __organizr2_add_auth_service
-    # curl -kL -X GET http: //traefik.chimere-harpie.org:9000/api/http/routers
-    # curl -kL -X GET http: //traefik.chimere-harpie.org:9000/api/http/middlewares
-
-    local __midname="${__service}-auth"
-    local __body="{http : {middlewares: {  } }}"
-    # curl -d "{}" -k -u mambo:mambo1 -X PUT https://traefik.chimere-harpie.org:9443/api/providers/rest
-
-
-    # middleware
-    #  {
-    #     "redirectScheme": {
-    #         "scheme": "https",
-    #         "port": "9443",
-    #         "permanent": true
-    #     },
-    #     "status": "enabled",
-    #     "usedBy": [
-    #         "http-catchall-web_admin@docker"
-    #     ],
-    #     "name": "redirect-secure-web_admin@docker",
-    #     "provider": "docker",
-    #     "type": "redirectscheme"
-    # }
+    [ "${__body}" = "" ] && docker run --network "${TANGO_APP_NETWORK_NAME}" --rm curlimages/curl:7.70.0 curl -u ${TRAEFIK_API_USER}:${TRAEFIK_API_HASH_PASSWORD} -X ${__http_command} -skL "${__url}" \
+        || docker run --network "${TANGO_APP_NETWORK_NAME}" --rm curlimages/curl:7.70.0 curl -d "${__body}" -u ${TRAEFIK_API_USER}:${TRAEFIK_API_HASH_PASSWORD} -X ${__http_command} -skL "${__url}"
 }
